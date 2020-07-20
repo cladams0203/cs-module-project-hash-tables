462961ec264cc14126ddd8c12072678be0ac44c1
cladams0203 <cladams0203@gmail.com>
2020-07-19 22:03:20 -0400

    updated exp seq problem

--- applications/expensive_seq/expensive_seq.py
+++ applications/expensive_seq/expensive_seq.py
@@ -9,9 +9,9 @@ def expensive_seq(x, y, z):
     # Your code here
     if x <= 0:
         return y + z
-    if x not in cache:
-        cache[x] = expensive_seq(x-1, y+1, z) + expensive_seq(x-2, y+2, z*2) + expensive_seq(x-3, y+3, z*3)
-    return cache[x]
+    if (x,y,z) not in cache:
+        cache[(x,y,z)] = expensive_seq(x-1, y+1, z) + expensive_seq(x-2, y+2, z*2) + expensive_seq(x-3, y+3, z*3)
+    return cache[(x,y,z)]
 
 if __name__ == "__main__":
     for i in range(10):

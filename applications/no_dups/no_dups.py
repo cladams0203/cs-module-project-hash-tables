b2e48a15811927f915f3cbeaf6b6e5b80e308431
cladams0203 <cladams0203@gmail.com>
2020-07-19 22:35:27 -0400

    finished no dups function

--- applications/no_dups/no_dups.py
+++ applications/no_dups/no_dups.py
@@ -1,7 +1,14 @@
 def no_dups(s):
     # Your code here
-
-
+    cache = {}
+    no_dupes = []
+    separator = ' '
+    s = s.split()
+    for word in s:
+        if word not in cache:
+            cache[word] = word
+            no_dupes.append(word)
+    return separator.join(no_dupes)
 
 if __name__ == "__main__":
     print(no_dups(""))
